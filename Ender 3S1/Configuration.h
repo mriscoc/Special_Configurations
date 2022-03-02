22e0b9a080c12a8b69a2e7f69f5f01f898452154
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2022-03-02 12:01:06 -0500

    Disables Experimental Subdivision of the grid by Catmull-Rom until fix incompatibility with bed visualizer

--- Ender 3S1/Configuration.h
+++ Ender 3S1/Configuration.h
@@ -1652,7 +1652,7 @@
     // Experimental Subdivision of the grid by Catmull-Rom method.
     // Synthesizes intermediate points to produce a more detailed mesh.
     //
-    #define ABL_BILINEAR_SUBDIVISION  // Ender3S1 Configs
+    //#define ABL_BILINEAR_SUBDIVISION  // Ender3S1 Configs, disabled by incompatibility with bed visualizer
     #if ENABLED(ABL_BILINEAR_SUBDIVISION)
       // Number of subdivisions between probe points
       #define BILINEAR_SUBDIVISIONS 5  // Ender3S1 Configs
