4b04a2ed31ebe6eb7ea96623278ca999bbf4b3fb
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2022-04-12 19:42:00 -0500

    Added V2 EZABL configuration files

---
+++ V2-EZABL.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# ------------------------------------------------------------------------------
+# Configurations generator script for the Professional Firmware
+# AutoLev from Substancia3D 
+# Author: Miguel A. Risco Castillo
+# URL: https://github.com/mriscoc/Marlin_Configurations
+# ------------------------------------------------------------------------------
+import CreateConfigs
+
+CreateConfigs.Generate('', ['422','EZABL'])
+CreateConfigs.Generate('', ['427','EZABL'])
+
+
+
