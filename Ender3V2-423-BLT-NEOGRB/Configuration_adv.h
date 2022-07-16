d12231377c721d76e433a53be7a491cb236da91d
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2022-07-16 18:33:44 -0500

    Update V2 NEOGRB please verify the value of NEOPIXEL_PIN

--- Ender3V2-423-BLT-NEOGRB/Configuration_adv.h
+++ Ender3V2-423-BLT-NEOGRB/Configuration_adv.h
@@ -599,7 +599,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1  // Ender Configs
+#define E0_AUTO_FAN_PIN -1  // Ender3V2 Configs
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
